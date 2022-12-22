6f471070a57e2f713a8ec04dfdf404bdee0066c8
JeanProton <jeanette.bergstrom@protonmail.com>
2022-12-22 17:28:26 +0100

    Tagit bort kommentarer

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app
     next();
   });
 
-
+/*-------------------GET------------------------*/
 app.get('/tasks', async (req, res) => {
   try {
     const tasks = await fs.readFile('./tasks.json');
@@ -23,10 +23,11 @@ app.get('/tasks', async (req, res) => {
   }
 });
 
-
+/*-------------------POST------------------------*/
 app.post('/tasks', async (req, res) => {
   try {
     const task = req.body;
+
     const listBuffer = await fs.readFile('./tasks.json');
     const currentTasks = JSON.parse(listBuffer);
     let maxTaskId = 1;
@@ -47,7 +48,7 @@ app.post('/tasks', async (req, res) => {
     res.status(500).send({ error: error.stack });
   }
 });
-
+/*-------------------DETELE------------------------*/
 app.delete('/tasks/:id', async (req, res) => {
   console.log(req);
   try {
@@ -55,31 +56,30 @@ app.delete('/tasks/:id', async (req, res) => {
     const listBuffer = await fs.readFile('./tasks.json');
     const currentTasks = JSON.parse(listBuffer);
     if (currentTasks.length > 0) {
-      /* Om det finns något i filen görs här en hel del i samma anrop: 
-      1. De befintliga uppgifterna (currentTasks), filtreras så att den uppgift med det id som skickades in filtreras bort och endast de uppgifter som inte hade det id:t är kvar.
-      2. Arrayen med alla uppgifter utom den med det id som skickades in görs om till en sträng med JSON.stringify
-      3. Denna sträng sparas slutgilgingen till filen tasks.json, så att det kommer att finnas en uppdaterad lista som inte längre innehåller uppgiften med det id som skickades in via url:en. */
       await fs.writeFile(
         './tasks.json',
         JSON.stringify(currentTasks.filter((task) => task.id != id))
       );
-      /* När den nya listan har skrivits till fil skickas ett success-response  */
       res.send({ message: `Uppgift med id ${id} togs bort` });
     } else {
-      /* Om det inte fanns något i filen sedan tidigare skickas statuskod 404. 404 används här för att det betyder "Not found", och det stämmer att den uppgift som man ville ta bort inte kunde hittas om listan är tom. Vi har dock inte kontrollerat inuti en befintlig lista om det en uppgift med det id som man önskar ta bort faktiskt finns. Det hade man också kunnat göra. */
       res.status(404).send({ error: 'Ingen uppgift att ta bort' });
     }
   } catch (error) {
-    /* Om något annat fel uppstår, skickas statuskod 500, dvs. ett generellt serverfel, tillsammans med information om felet.  */
     res.status(500).send({ error: error.stack });
   }
 });
 
 /***********************Labb 2 ***********************/
-/* Här skulle det vara lämpligt att skriva en funktion som likt post eller delete tar kan hantera PUT- eller PATCH-anrop (du får välja vilket, läs på om vad som verkar mest vettigt för det du ska göra) för att kunna markera uppgifter som färdiga. Den nya statusen - completed true eller falase - kan skickas i förfrågans body (req.body) tillsammans med exempelvis id så att man kan söka fram en given uppgift ur listan, uppdatera uppgiftens status och till sist spara ner listan med den uppdaterade uppgiften */
+/* Här skulle det vara lämpligt att skriva en funktion som likt post eller delete tar kan hantera PUT- eller PATCH-anrop (du får välja vilket
+ läs på om vad som verkar mest vettigt för det du ska göra) för att kunna markera uppgifter som färdiga. Den nya statusen - completed true eller falase - 
+ kan skickas i förfrågans body (req.body) tillsammans med exempelvis id så att man kan söka fram en given uppgift ur listan, 
+ uppdatera uppgiftens status och till sist spara ner listan med den uppdaterade uppgiften */
 
-/* Observera att all kod rörande backend för labb 2 ska skrivas i denna fil och inte i app.node.js. App.node.js är bara till för exempel från lektion 5 och innehåller inte någon kod som används vidare under lektionerna. */
+/* Observera att all kod rörande backend för labb 2 ska skrivas i denna fil och inte i app.node.js. App.node.js är bara till för exempel från lektion 5 och innehåller 
+inte någon kod som används vidare under lektionerna. */
 /***********************Labb 2 ***********************/
 
-/* Med app.listen säger man åte servern att starta. Första argumentet är port - dvs. det portnummer man vill att servern ska köra på. Det sattes till 5000 på rad 9. Det andra argumentet är en anonym arrow-funktion som körs när servern har lyckats starta. Här skrivs bara ett meddelande ut som berättar att servern kör, så att man får feedback på att allt körts igång som det skulle. */
+/* Med app.listen säger man åte servern att starta. Första argumentet är port - dvs. det portnummer man vill att servern ska köra på. 
+Det sattes till 5000 på rad 9. Det andra argumentet är en anonym arrow-funktion som körs när servern har lyckats starta. 
+Här skrivs bara ett meddelande ut som berättar att servern kör, så att man får feedback på att allt körts igång som det skulle. */
 app.listen(PORT, () => console.log('Server running on http://localhost:5000'));
